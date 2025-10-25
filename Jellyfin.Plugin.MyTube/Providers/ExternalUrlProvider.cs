961677c7d191a16d251807fd3e5a79365adcf1a5
Topbcy <dissyu923@gmail.com>
2025-10-26 07:12:34 +0800

    Update ExternalUrlProvider.cs

--- Jellyfin.Plugin.MyTube/Providers/ExternalUrlProvider.cs
+++ Jellyfin.Plugin.MyTube/Providers/ExternalUrlProvider.cs
@@ -1,26 +1,25 @@
 #if !__EMBY__
-using Jellyfin.Plugin.MyTube.ExternalIds;
+using Jellyfin.Plugin.MetaTube.ExternalIds;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 
-namespace Jellyfin.Plugin.MyTube.Providers;
+namespace Jellyfin.Plugin.MetaTube.Providers;
 
 public class ExternalUrlProvider : IExternalUrlProvider
 {
-    public string Name => Plugin.Instance.Name;
+    public string Name => Plugin.ProviderName;
 
     public IEnumerable<string> GetExternalUrls(BaseItem item)
     {
-        if (item.TryGetProviderId(Plugin.Instance.Name, out var pid))
+        if (item.TryGetProviderId(Plugin.ProviderId, out var pid))
         {
             switch (item)
             {
                 case Movie:
                     yield return string.Format(new MovieExternalId().UrlFormatString, pid);
                     break;
-
                 case Person:
                     yield return string.Format(new ActorExternalId().UrlFormatString, pid);
                     break;
